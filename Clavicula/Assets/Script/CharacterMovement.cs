17c992d9761b5ce30b00112c67c74bedf6d6dc64
ukjolie2 <ukjolie2@gmail.com>
2017-07-11 13:44:26 -0700

    Added Idle animations
    Added Idle animations for Solomon

--- Clavicula/Assets/Script/CharacterMovement.cs
+++ Clavicula/Assets/Script/CharacterMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour {
 
     public float walkSpeed = 10;
+    private int direction = 0;
     private Rigidbody2D player;
     private AnimationController2D animate;
 	// Use this for initialization
@@ -24,6 +25,7 @@ public class CharacterMovement : MonoBehaviour {
             if (!Input.GetButton("Vertical"))
             {
                 animate.setAnimation("WalkLeft");
+                direction = 1;
             }
         }
         else if(Input.GetAxis("Horizontal") > 0)
@@ -32,32 +34,51 @@ public class CharacterMovement : MonoBehaviour {
             if (!Input.GetButton("Vertical"))
             {
                 animate.setAnimation("WalkRight");
+                direction = 3;
             }
         }
         if(Input.GetAxis("Vertical") > 0)
         {
             velocity.y = walkSpeed;
             animate.setAnimation("WalkUp");
+            direction = 2;
         }
         else if(Input.GetAxis("Vertical") < 0)
         {
             velocity.y = -walkSpeed;
             animate.setAnimation("WalkDown");
+            direction = 0;
         }
-        velocity.x *= 0.85f;
-        velocity.y *= 0.85f;
+        velocity.x *= 0.6f;
+        velocity.y *= 0.6f;
+        if (velocity.x < 2 && velocity.y < 2 && velocity.x > -2 && velocity.y > -2)
+        {
+            switch (direction)
+            {
+                case 0:
+                    animate.setAnimation("IdleDown");
+                    break;
+                case 1:
+                    animate.setAnimation("IdleLeft");
+                    break;
+                case 2:
+                    animate.setAnimation("IdleUp");
+                    break;
+                case 3:
+                    animate.setAnimation("IdleRight");
+                    break;
+            }
+        }
         player.velocity = velocity;
 	}
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        print("entered");
+    { 
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        print("exit");
+        //print("exit");
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        print("stay");
     }
 }
